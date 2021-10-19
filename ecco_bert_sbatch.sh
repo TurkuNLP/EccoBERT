221bc79f576ca6b60533c940d2e22b7742bf7b0a
Filip Ginter <ginter@mahti-login1.mahti.csc.fi>
2021-10-19 22:13:39 +0300

    Revert "fixin stuff"
    This reverts commit b3354747d56ba07f45a6df644675c1d89140d978.

--- ecco_bert_sbatch.sh
+++ ecco_bert_sbatch.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
 #SBATCH --account=Project_2005072
-#SBATCH --time=09:00:00
+#SBATCH --time=01:00:00
 ##SBATCH --time=00:15:00
 #SBATCH --nodes=2
-#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks=8
 #SBATCH --mem=96G
 #SBATCH --partition=gpumedium
-##SBATCH --partition=gputest
 #SBATCH --gres=gpu:a100:4,nvme:8
-##SBATCH --gres=gpu:a100:2,nvme:8
 #SBATCH --cpus-per-task=32
 
-module purge
 module load pytorch/1.9
-export SING_IMAGE=/appl/soft/ai/singularity/images/pytorch_1.9.0_csc_custom.sif
-
 export TORCH_EXTENSIONS_DIR=$LOCAL_SCRATCH
 export TMPDIR=$LOCAL_SCRATCH
 export PYTORCH_PRETRAINED_BERT_CACHE=$TMPDIR
-#export NCCL_IB_DISABLE=1
-
-NGPUS=4
-NNODES=2
+export NCCL_IB_DISABLE=1
 
 TOKENIZER=$1 ## tokenizer vocabulary to use (x1, x2, x2CLS)
 TRAINDATA=$2 ## training data to use
@@ -30,8 +22,8 @@ OUTDIR=$4 ## directory to which the model is saved
 
 if [ $# -eq 4 ]
 then
-    srun python ecco_bert.py --gpus $NGPUS --nodes $NNODES $TOKENIZER $TRAINDATA $DEVDATA $OUTDIR
+    srun python ecco_bert.py $TOKENIZER $TRAINDATA $DEVDATA $OUTDIR
 elif [ $# -eq 5 ]
 then
-    srun python ecco_bert.py --gpus $NGPUS --nodes $NNODES $TOKENIZER $TRAINDATA $DEVDATA $OUTDIR --load_checkpoint $5
+    srun python ecco_bert.py $TOKENIZER $TRAINDATA $DEVDATA $OUTDIR --load_checkpoint $5
 fi
